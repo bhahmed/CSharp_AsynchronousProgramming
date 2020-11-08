b9a7b596811b2ff0cc89ac667e39ce1583dada2f
AhmedBenHmida <ahmed.benhmida@newaccess.ch>
2020-11-08 17:38:18 +0100

    Normal execute - Async execute

--- AsyncAwaitSample/AsyncAwaitSample/MainWindow.xaml.cs
+++ AsyncAwaitSample/AsyncAwaitSample/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,86 @@ namespace AsyncAwaitSample
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ExecuteSync_OnClick(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            RunDownloadSync();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: {elapsedMs}";
+        }
+
+        private async void ExecuteAsync_OnClick(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            await RunDownloadAsync();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: {elapsedMs}";
+        }
+
+        private List<string> PrepData()
+        {
+            List<string> output = new List<string>();
+
+            resultsWindow.Text = "";
+
+            output.Add("https://www.yahoo.com");
+            output.Add("https://www.google.com");
+            output.Add("https://www.microsoft.com");
+            output.Add("https://www.cnn.com");
+            output.Add("https://www.codeproject.com");
+            output.Add("https://www.stackoverflow.com");
+
+            return output;
+        }
+
+
+        private void RunDownloadSync()
+        {
+            List<string> websites = PrepData();
+
+            foreach (var site in websites)
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        private async Task RunDownloadAsync()
+        {
+            List<string> websites = PrepData();
+
+            foreach (var site in websites)
+            {
+                WebsiteDataModel results = await Task.Run(() => DownloadWebsite(site));
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        private WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsieUrl = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL);
+
+            return output;
+        }
+
+        private void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            resultsWindow.Text +=
+                $"{data.WebsieUrl} downloaded: {data.WebsiteData.Length} characters long.{Environment.NewLine}";
         }
     }
 }
---
+++ AsyncAwaitSample/AsyncAwaitSample/WebsiteDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitSample
+{
+    public class WebsiteDataModel
+    {
+        public string WebsieUrl { get; set; } = "";
+
+        public string WebsiteData { get; set; } = "";
+    }
+}
